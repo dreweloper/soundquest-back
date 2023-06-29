e93a71690d7e2b07027d4b5e87842eb05bdf0da4
dreweloper <dreweloper@gmail.com>
2023-06-29 10:37:57 +0200

    Documentación básica en tokenController

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,17 +1,38 @@
-
+/**
+ * Request an access token.
+ * @function getSpotifyToken
+ * @async
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ * @returns {Promise}
+ */
 const getSpotifyToken = async (req, res) => {
+
+    /**
+     * @type {String} The token endpoint URI.
+     */
+    const url = 'https://accounts.spotify.com/api/token';
+
+    /**
+     * Fetch options.
+     * @typedef {Object} options
+     * @property {String} method HTTP verb for the request.
+     * @property {Object} body URLSearchParams object that contains the Client ID and Client Secret, along with the grant_type parameter set to client_credentials.
+     * @property {Object} headers Content-type header set to the application/x-www-form-urlencoded value.
+     */
+    const options = {
+        method: 'POST',
+        body: new URLSearchParams({
+            'grant_type': 'client_credentials',
+            'client_id': process.env.CLIENT_ID,
+            'client_secret': process.env.CLIENT_SECRET
+        }),
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    };
 
     try {
 
-        const request = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            body: new URLSearchParams({
-                'grant_type': 'client_credentials',
-                'client_id': process.env.CLIENT_ID,
-                'client_secret': process.env.CLIENT_SECRET
-            }),
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        });
+        const request = await fetch(url, options);
 
         const response = await request.json();
 
